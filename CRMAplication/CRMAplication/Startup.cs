d406a9addd171213ef3e1b0428541981674ac1cc
marcin wiktor <witar1@gmail.com>
2018-03-29 18:38:44 +0200

    init

---
+++ CRMAplication/CRMAplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRMAplication.Startup))]
+namespace CRMAplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
