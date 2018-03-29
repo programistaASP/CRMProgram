f5863a18ac1026678a9f3911c2c6d352bc6f6bcb
marcin wiktor <witar1@gmail.com>
2018-03-29 20:04:27 +0200

    dodanie EmailService

---
+++ CRMAplication/CRMAplication/Interfaces/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using CRMAplication.Models;
+using System.Net.Mail;
+
+namespace CRMAplication.Interfaces
+{
+    public interface IEmailService
+    {
+        MailMessage CreateMailMessage(Car carmodel);
+        void SendEmail(MailMessage mailMessage);
+    }
+}
---
+++ CRMAplication/CRMAplication/Service/EmailService.cs
@@ -0,0 +1,46 @@
+﻿using CRMAplication.Interfaces;
+using CRMAplication.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace CRMAplication.Service
+{
+    public class EmailService: IEmailService
+    {
+        private SmtpClient _smtpClient;
+        private const string _login = "witar1@gmail.com";
+        private const string _pass = "Heroes840715";
+
+        public EmailService()
+        {
+            Initialize();
+        }
+        private void Initialize()
+        {
+            _smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                UseDefaultCredentials = true,
+                Credentials = new NetworkCredential(_login, _pass)
+            };
+        }
+        public MailMessage CreateMailMessage(Car carmodel)
+        {
+            return new MailMessage
+            {
+                Sender = new MailAddress(_login),
+                From = new MailAddress(_login),
+                To = { _login },
+                Subject = $"Unauthorized person added car at {carmodel.DateCreate}",
+                Body = "Unauthorized person added car",
+                IsBodyHtml = true
+            };
+        }
+        public void SendEmail(MailMessage mailMessage)
+        {
+            _smtpClient.Send(mailMessage);
+        }
+    }
+}
