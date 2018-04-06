38fe69ac73e4188901d306b9c36798448b4e9975
marcin wiktor <witar1@gmail.com>
2018-04-06 18:59:34 +0200

    normalnie załamka

--- CRMAplication/CRMAplication/Models/IdentityModels.cs
+++ CRMAplication/CRMAplication/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace CRMAplication.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ServisVolkswagena", throwIfV1Schema: false)
         {
         }
 
